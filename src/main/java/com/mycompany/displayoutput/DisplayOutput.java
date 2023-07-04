3f934f95d8d90d92ab034e4d0583bb1fb317469c
Raseealmanea <138548575+Raseealmanea@users.noreply.github.com>
2023-07-04 07:19:16 +0300

    First Commit

---
+++ src/main/java/com/mycompany/displayoutput/DisplayOutput.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.displayoutput;
+
+/**
+ *
+ * @author slymanalmny
+ */
+public class DisplayOutput {
+
+    public static void main(String[] args) {
+    System.out.println("He said that \"Iam from Egypt\" But lives in \"KSA\" ");
+    System.out.println("He said that \n Iam from Egypt \n But lives in KSA");
+    System.out.println("the file located in d:\\javaproject\\tryoutput");
+    System.out.println("work days Are \n s \t m \t t \t w \t ");
+    }
+}
